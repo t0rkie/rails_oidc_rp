b3e16ee04c760ce5d25b9100c5176fae0625ecfc
秋吉時弥 <akiyoshi.tokiya@iticket.co.jp>
2022-06-15 13:30:11 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  # get 'home/index'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
